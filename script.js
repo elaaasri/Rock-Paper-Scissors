ea469119df9f37c2990d57735ecedb63fd88d770
m0bu <anas_elasri@outlook.fr>
2023-02-13 16:52:22 +0100

    v1 completed.

--- script.js
+++ script.js
@@ -2,33 +2,45 @@ function getComputerChoice() {
     let randomChoice = ["Rock", "Paper", "Scissors"];
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 };
-getComputerChoice();
-
-let playerSelection = prompt('choose?','');
-let caseInsensitive = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
-    console.log(`- Player: ${playerSelection}`);
-    console.log(`- Computer: ${computerSelection}`);
-    // Conditional Ternary Operator:
-    playerSelection === computerSelection 
-    ? console.log("Its A Tie!") 
-    : playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock" 
-    ? console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`) 
-    : playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper" 
-    ? console.log(`You Win! ${playerSelection} beats ${computerSelection}!`) 
-    : playerSelection === null || playerSelection === ''
-    ? console.log("Make Your Choice!") 
-    : console.log("Wrong Input!");
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    console.log(`- Player : ${playerSelection}`);
+    console.log(`- Computer : ${computerSelection}`);
+        if (playerSelection === computerSelection) {
+            return "Its A Tie!";
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper") {
+            return `win`;
+        } else {
+            return `loose`;
+        }
 }
 
-let counter = 1;
+let Pscore = 0;
+let Cscore = 0;
 function game() {
-    console.log("#".repeat(15));
     for (let i = 0; i < 5; i++) {
-        console.log(`###--ROUND[${counter++}]:`);
-        playRound(caseInsensitive, getComputerChoice());
-        console.log("#".repeat(15));
+        console.log(`#Round [${i + 1}]`)
+        let playerSelection = prompt('choose','');
+        let computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result === "win") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
+            console.log(`=> Player Scored ${++Pscore}`);
+        } else if (result === "loose"){
+            console.log(`You Loose! ${computerSelection} beats ${playerSelection}`)
+            console.log(`=> Computer Scored ${++Cscore}`);
+        } else {
+            console.log("Its A tie!");
+        }
     }
-}
+    console.log(`###-The Final Result : ${Pscore} - ${Cscore}`)
+    if (Pscore > Cscore) {
+            console.log("###---The Winner is The PLAYER!");
+        } else if (Cscore > Pscore) {
+            console.log("- The Winner is The COMPUTER!");
+        } else {
+            console.log("- Its A Tie!");
+        }
+}  
 game();
