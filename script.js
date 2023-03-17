5fbd0f980bafaf507304f73d17dbd1daf41467db
elaaasri <anas_elasri@outlook.fr>
2023-03-17 20:56:05 +0100

    .

--- script.js
+++ script.js
@@ -1,46 +1,83 @@
-let btns = document.querySelectorAll("button");
-let rockBtn = document.querySelector(".Rock");
-let paperBtn = document.querySelector(".Paper")
-let scissorsBtn = document.querySelector(".Scissors");
-
+   // Random Computer Choice :
 function getComputerChoice() {
     let randomChoice = ["Rock", "Paper", "Scissors"];
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 };
+    // Selecting Elements :
+let btns = document.querySelectorAll("button");
+let ResultContainer = document.querySelector(".Results-Container");
+
+// Creating New Elements :  
+let finalResult  = document.createElement('h1');
+let winnerResult = document.createElement("h3");
+let playerScore = document.createElement("div");
+let computerScore = document.createElement("div");
+let endResult = document.createElement("h1");
+// Setting Attributes : 
+finalResult.setAttribute('class', 'Final-Result');
+winnerResult.setAttribute('class', "Winner-Result");
+playerScore.setAttribute("class", "Score");
+computerScore.setAttribute("class", "Score");
+endResult.setAttribute("class", "End-Game");
+// Appending :
+ResultContainer.appendChild(finalResult);
+ResultContainer.appendChild(winnerResult);
+ResultContainer.appendChild(playerScore);
+ResultContainer.appendChild(computerScore);
+ResultContainer.appendChild(playerScore);
+ResultContainer.appendChild(computerScore);
+ResultContainer.appendChild(endResult);
+// Elements Start Textcontent: 
+finalResult.textContent = "Choose You're Weapon";
+winnerResult.textContent = "First to Score 5 Wins The Game!";
+playerScore.textContent = "Player Score : 0";
+computerScore.textContent = "Computer Score : 0";
+
+let pScore = 0;
+let cScore = 0; 
 
 function playRound(playerSelection,computerSelection) {
-    // creating new elements : 
-    let finalResult  = document.createElement('h1');
-    let playerResult = document.createElement('div');
-    let computerResult = document.createElement("div");
-    // setting attributes : 
-    finalResult.setAttribute('class', 'Final-Result');
-    playerResult.setAttribute('class', 'Player-Result');
-    computerResult.setAttribute("class","Computer-Result")
-    // appending :
-    document.body.appendChild(finalResult);
-    document.body.appendChild(playerResult);
-    document.body.appendChild(computerResult);
-
     if (playerSelection.value === computerSelection) {
         finalResult.textContent = `IT'S A TIE!`;
-        playerResult.textContent = `Player : ${playerSelection.value}`;
-        computerResult.textContent = `Computer : ${computerSelection}`;
+        winnerResult.textContent = `${playerSelection.value} Ties with ${computerSelection}`
+        playerScore.textContent = `Player Score : ${pScore}`;
+        computerScore.textContent = `Computer Score : ${cScore}`;
     } else if (playerSelection.value === "Rock" && computerSelection === "Scissors" || playerSelection.value === "Paper" && computerSelection === "Rock" || playerSelection.value === "Scissors" && computerSelection === "Paper") {
         finalResult.textContent ="You Won!";
-        playerResult.textContent = `Player : ${playerSelection.value}`;
-        computerResult.textContent = `Computer : ${computerSelection}`;
+        winnerResult.textContent = `${playerSelection.value} beats ${computerSelection}`
+        playerScore.textContent = `Player Score : ${++pScore}`
+
     } else {
         finalResult.textContent ="You Lost!";
-        playerResult.textContent = `Player : ${playerSelection.value}`;
-        computerResult.textContent = `Computer : ${computerSelection}`;
+        winnerResult.textContent = `${computerSelection} beats ${playerSelection.value}`
+        computerScore.textContent = `Computer Score : ${++cScore}`;
+    };
+
+    console.log(pScore);
+    console.log(cScore);
+
+    if (pScore === 5) {
+        endResult.textContent = "YOU WON!";
+    } else if (cScore === 5) {
+        endResult.textContent = "COMPUTER WON!";
     };
 };
 
+    // Restart Button :
+let restartButton = document.createElement("button");
+restartButton.setAttribute("class", "Restart");
+restartButton.textContent = "Play Again";
+document.body.appendChild(restartButton);
+restartButton.onclick = function(e) {
+    window.location.reload();
+};
+
+    // Buttons Clicked : 
 let ArrFromBtns = Array.from(btns);
 ArrFromBtns.forEach(btn => btn.addEventListener("click", function() {
-    playRound(this.classList, getComputerChoice())
-}))
+    playRound(this.classList, getComputerChoice());
+}));
+
 
 // let Pscore = 0;
 // let Cscore = 0;
